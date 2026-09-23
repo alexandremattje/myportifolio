0ddeb8b709668dda1019187975281cd39ff4e8bb
Alexandre Mattje <amattje@gmail.com>
2026-09-23 11:15:06 -0300

    feat: add Spring Boot application entrypoint

---
+++ backend/src/main/kotlin/com/alexandremattje/myportifolio/MyPortifolioApplication.kt
@@ -0,0 +1,11 @@
+package com.alexandremattje.myportifolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyPortifolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyPortifolioApplication>(*args)
+}
